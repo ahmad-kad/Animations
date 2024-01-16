b750f1ba35f4c01a2d1881c59461003db3883b64
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-16 13:38:26 -0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by ahmad kaddoura on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
